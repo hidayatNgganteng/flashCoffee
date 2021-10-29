69d0ba20821c89d98180ea070a64ccb559a3354d
hidayatNgganteng <dayatznur@gmail.com>
2021-10-29 14:44:38 +0700

    fixes minor bug splash ios & modify splash screen android

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import { View, Text } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
+
+export default function App() {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  )
+}
