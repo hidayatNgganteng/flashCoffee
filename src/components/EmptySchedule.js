0bcf3dece9bf3b0a921d1cebae29d239a0f98231
hidayatNgganteng <dayatznur@gmail.com>
2021-11-03 15:06:00 +0700

    make empty component more modular

--- src/components/EmptySchedule.js
+++ src/components/EmptySchedule.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import theme from '../theme'
 
 const EmptySchedule = () => {
   return (
-    <View>
-      <Text>holla</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>NO SCHEDULE</Text>
     </View>
   )
 }
 
 export default EmptySchedule
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: theme.colors.gray,
+    borderStyle: 'dashed',
+    height: 85
+  },
+  label: {
+    fontSize: theme.fonts.normal,
+    fontWeight: 'bold'
+  }
+})
--- src/components/ScheduleCard.js
+++ src/components/ScheduleCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import theme from '../theme'
 import HeadlineSchedule from './HeadlineSchedule'
+import EmptySchedule from './EmptySchedule'
 
 const ScheduleCard = ({ day, dateVal, schedule }) => {
   return (
@@ -10,15 +11,13 @@ const ScheduleCard = ({ day, dateVal, schedule }) => {
         <Text style={styles.dayStyle}>{ day }</Text>
         <Text style={styles.dateStyle}>{ dateVal }</Text>
       </View>
-      <TouchableOpacity onPress={schedule.onPress} style={styles.card}>
+      {/* <TouchableOpacity onPress={schedule.onPress} style={styles.card}>
         <HeadlineSchedule
           title={schedule.title}
           time={schedule.time}
           isWithTodayLabel={false}/>
-      </TouchableOpacity>
-      {/* <View style={styles.emptySchedule}>
-        <Text style={styles.emptyScheduleLabel}>NO SCHEDULE</Text>
-      </View> */}
+      </TouchableOpacity> */}
+      <EmptySchedule />
     </View>
   )
 }
@@ -54,19 +53,5 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     height: 85,
     justifyContent: 'center'
-  },
-  emptySchedule: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    flex: 1,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: theme.colors.gray,
-    borderStyle: 'dashed',
-    height: 85
-  },
-  emptyScheduleLabel: {
-    fontSize: theme.fonts.normal,
-    fontWeight: 'bold'
   }
 })
